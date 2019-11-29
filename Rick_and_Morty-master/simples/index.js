9d1cfb23e7f4fe03916fa51a1da87b2ab4fcb237
naraxhevila <54638977+naraxhevila@users.noreply.github.com>
2019-11-29 15:41:30 -0300

    Add files via upload

---
+++ Rick_and_Morty-master/simples/index.js
@@ -0,0 +1,206 @@
+class Http {
+    constructor() {}
+
+    xmlRequest(metodo, url) {
+
+        return new Promise((resolve, reject) => {
+            var request = new XMLHttpRequest()
+            request.open(metodo, url);
+            request.send();
+
+            request.onreadystatechange = () => {
+                if (request.readyState == 4) {
+                    if (request.status == 200) {
+                        resolve(JSON.parse(request.responseText))
+                    } else {
+                        reject(request.status)
+                    }
+                }
+            }
+        })
+    }
+
+  
+
+    personagem() {
+            this.xmlRequest('GET', 'https://rickandmortyapi.com/api/character/')
+                .then(dados => {
+                        document.querySelector("#tabela").innerHTML = `
+                <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Foto</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Gênero</th>
+                        <th scope="col">Espécie</th>
+                        <th scope="col">Localização</th>
+                    </tr>
+                </thead>
+                <tbody>
+                ${
+                    dados.results.map(item =>{
+            return ` 
+             <a class="dropdown-item tabela" onclick="request.pegar('${item.name}')"> ${item.name} </a>`
+            }).join("")
+           
+            } `
+              
+                
+            }).catch(erro =>{
+                document.querySelector('#tabela').innerHTML = `<h2>Error! ${erro} </h2>`
+            })
+            
+
+}
+
+pegar(id) {
+    this.xmlRequest('GET', 'https://rickandmortyapi.com/api/character/').then(dados => {
+ 
+        dados.results.forEach(topico => {
+            if (topico.name === id) {
+                console.log(topico.name)
+                document.querySelector("#tabela").innerHTML = `
+        <table class="table">
+    
+        <thead>
+            <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Status</th>
+                <th scope="col">genero</th>
+                <th scope="col">Imagem</th>
+               
+            </tr>
+        </thead>
+        <tbody id="tabela2"></tbody>
+    
+    <tbody>
+            ${  ` <tr>
+                    <td>${topico.name}</td>
+                    <td>${topico.status}</td>
+                    <td>${topico.gender}</td>
+                    <td><img src="${topico.image}" alt="..." class="img-thumbnail" height="50" width="50"></td>
+                   
+                    </tr>`
+            }
+            </tbody>
+        </table>`
+                
+            }
+        })
+    })
+}
+
+pequisanome(event){
+    event.preventDefault();
+    let $ = document.querySelector.bind(document);
+    var nome = $(".nome").value;
+
+    this.xmlRequest('GET', 'https://rickandmortyapi.com/api/character/').then(dados => {
+       
+        dados.results.forEach(pesquisa => { 
+            if (pesquisa.name === nome ){
+      document.querySelector("#tabela").innerHTML = `
+        <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Status</th>
+                <th scope="col">genero</th>
+                <th scope="col">Imagem</th>
+               
+            </tr>
+        </thead>
+        <tbody id="tabela2"></tbody>
+    
+    <tbody>
+            ${  ` <tr>
+                    <td>${pesquisa.name}</td>
+                    <td>${pesquisa.status}</td>
+                    <td>${pesquisa.gender}</td>
+                    <td><img src="${pesquisa.image}" alt="..." class="img-thumbnail" height="50" width="50"></td>
+                   
+                    </tr>`
+            }
+            </tbody>
+        </table>`
+    return
+          }
+          else{
+            console.log("tá fazendo o if errado sua burra")
+        }
+        })
+       
+    })
+}
+
+    
+ epsodios() {
+
+    this.xmlRequest('GET','https://rickandmortyapi.com/api/episode').then(dados =>{
+        document.querySelector("#tabela").innerHTML = `
+        <table class="table">
+    
+        <thead>
+            <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Data de Lançamento</th>
+                <th scope="col">Episódio</th>
+                <th scope="col">Criado</th>
+            </tr>
+        </thead>
+        <tbody id="tabela2"></tbody>
+    
+    <tbody>
+            ${ dados.results.map(item =>{
+                return  ` <tr>
+                    <td>${item.name}</td>
+                    <td>${item.air_date}</td>
+                    <td>${item.episode}</td>
+                    <td>${item.created}</td>
+                    </tr>`
+                }).join('')}
+            </tbody>
+        </table>`
+    }).catch(erro=>{
+        document.querySelector("#tabela").innerHTML = `<h2>Error! ${erro} </h2> 
+        `
+    })
+
+    
+}
+
+
+localizacao(){
+    this.xmlRequest('GET','https://rickandmortyapi.com/api/location').then(dados =>{
+        document.querySelector("#tabela").innerHTML = `
+        <table class="table">
+    
+        <thead>
+            <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Data de Lançamento</th>
+                <th scope="col">Episódio</th>
+                <th scope="col">Criado</th>
+            </tr>
+        </thead>
+        <tbody id="tabela2"></tbody>
+    
+    <tbody>
+            ${ dados.results.map(item =>{
+                return  ` <tr>
+                    <td>${item.name}</td>
+                    <td>${item.type}</td>
+                    <td>${item.dimension}</td>
+                    <td>${item.created}</td>
+                    </tr>`
+                }).join('')}
+            </tbody>
+        </table>`
+    }).catch(erro=>{
+        document.querySelector("#tabela").innerHTML = `<h2>Error! ${erro} </h2> 
+        `
+    })
+}
+
+}
